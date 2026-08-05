10007e9f4f1f27e1428fc0a2ad83ccdd001e2563
MuhammadHaroonHK <haroonhk059@gmail.com>
2026-08-05 16:46:02 +0500

    Authorization added by using Role-base-access

--- server/src/modules/auth/auth.routes.ts
+++ server/src/modules/auth/auth.routes.ts
@@ -3,6 +3,8 @@ import { authController } from "./auth.controller";
 import { validate } from "../../middleware/validate";
 import { loginSchema } from "./auth.validation";
 import { authenticate } from "../../middleware/authenticate";
+import { UserRole } from "@prisma/client";
+import { authorize } from "../../middleware/authorize";
 
 const router = Router();
 
@@ -16,6 +18,18 @@ router.get(
   "/me",
   authenticate,
   authController.me.bind(authController)
+);
+
+router.get(
+  "/admin-only",
+  authenticate,
+  authorize(UserRole.SUPER_ADMIN),
+  (req, res) => {
+    return res.json({
+      success: true,
+      message: "Welcome Super Admin!",
+    });
+  }
 );
 
 export default router;
