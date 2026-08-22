cc0f5ac8784929c03e6d9fe3967b0a376df0de9a
MuhammadHaroonHK <haroonhk059@gmail.com>
2026-08-22 10:08:01 +0500

    Now image upload issue become resolved and all uploaded image loaded correctly

--- client/lib/api.ts
+++ client/lib/api.ts
@@ -3,9 +3,6 @@ import { authStorage } from "@/features/auth/services/auth-storage";
 
 const api = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_URL,
-  headers: {
-    "Content-Type": "application/json",
-  },
 });
 
 api.interceptors.request.use((config) => {
@@ -13,6 +10,19 @@ api.interceptors.request.use((config) => {
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  /*
+   * Let Axios/browser automatically set the correct
+   * Content-Type header for FormData requests.
+   *
+   * For normal objects, Axios will automatically use
+   * application/json.
+   */
+  if (
+    config.data instanceof FormData
+  ) {
+    delete config.headers["Content-Type"];
   }
 
   return config;
